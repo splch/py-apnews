5aa0976280fb79546bbd1ab04ecbe7fcf5290255
Spencer Churchill <spence@duck.com>
2023-05-17 23:56:40 -0700

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="apnews",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple web scraper for Associated Press",
     author="Spencer Churchill",
     author_email="spence@duck.com",
